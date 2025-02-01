97eedc5e28ab3a6ad62384ebb35365236317c899
Nikita hramchenko <nikita.hramchenko@mail.ru>
2025-02-01 18:22:36 +0700

    create settings.py

--- game.py
+++ game.py
@@ -1,24 +1,30 @@
-import sys
-import pygame
+import sys, pygame
+from settings import Settings
+
 
 class AlienInvasion:
     """Класс инициализации игры"""
+
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption('Alien Invasion')
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
     def run_game(self):
         """Запускаем цикл игры"""
         while True:
-            #Следим за клавой и мышкой
+            # Следим за клавой и мышкой
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # Назначаем цвет фона
+            self.screen.fill(self.settings.bg_color)
+            # Последний прорисованный экран
             pygame.display.flip()
 
+
 if __name__ == '__main__':
-    #Создаем экземпляр класса
+    # Создаем экземпляр класса
     ai = AlienInvasion()
-    #Запускаем игру
+    # Запускаем игру
     ai.run_game()
