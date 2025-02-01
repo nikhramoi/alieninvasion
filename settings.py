c81b6d97d49a3fa2c3d63c2201366d037092e233
Nikita hramchenko <nikita.hramchenko@mail.ru>
2025-02-01 20:47:37 +0700

    moving right-left

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ class Settings():
     def __init__(self):
         """Инициализация параметров игры"""
         # Настройки экрана
+        self.ship_speed_factor = 2.5
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (200, 200, 200)
--- ship.py
+++ ship.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, settings
 
 
 class Ship():
@@ -6,19 +6,27 @@ class Ship():
 
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         # Загружаем изображение корабля
         self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
         # Новые корабли у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
+        # Сохранение вещественной координаты центра корабля
+        self.x = float(self.rect.x)
         #флаг перемещения
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         #Обновляет позицию с учетом флага
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed_factor
+        if self.moving_left:
+            self.x -= self.settings.ship_speed_factor
+        #обновление позиции по x траектории
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
